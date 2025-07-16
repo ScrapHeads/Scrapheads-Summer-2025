0821efb12b47fff9b423fd4557c3b73add682f60
tebbe21485 <157887121+tebbe21485@users.noreply.github.com>
2025-07-16 15:36:07 -0500

    Added drive system

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/DriveContinous.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode.Commands;
+
+import com.acmerobotics.roadrunner.PoseVelocity2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.arcrobotics.ftclib.command.CommandBase;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+
+import org.firstinspires.ftc.teamcode.Subsystems.Drivetrain;
+
+public class DriveContinous extends CommandBase {
+    private Drivetrain drivetrain;
+    private GamepadEx driver;
+    private double speed;
+
+    public DriveContinous(Drivetrain drivetrain, GamepadEx driver, double speed) {
+        this.drivetrain = drivetrain;
+        this.driver = driver;
+        this.speed = speed;
+
+        addRequirements(drivetrain);
+    }
+
+//    @Override
+//    public void initialize(){
+//
+//    }
+
+    @Override
+    public void execute(){
+        PoseVelocity2d pose = new PoseVelocity2d(
+                new Vector2d(
+                        driver.getLeftY() * speed,
+                        -driver.getLeftX() * speed
+                        ),
+                -driver.getRightX() * speed); // Normally we wouldn't need to invert
+        drivetrain.setDrivePowers(pose);
+    }
+
+    @Override
+    public void end(boolean isInterrupted){
+        PoseVelocity2d pose = new PoseVelocity2d(
+                new Vector2d(
+                        0,
+                        0
+                ),
+                0);
+        drivetrain.setDrivePowers(pose);
+    }
+}
